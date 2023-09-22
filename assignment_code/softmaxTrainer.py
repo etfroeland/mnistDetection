34de56554004e076ea7326f26f77b2338ebe560d
etfroeland <etfroeland@gmail.com>
2023-09-22 22:00:43 +0200

    Fix in notebook

--- assignment_code/softmaxTrainer.py
+++ assignment_code/softmaxTrainer.py
@@ -1,9 +1,9 @@
 import numpy as np
 import utils
 import matplotlib.pyplot as plt
-from assignment_code.softmaxModel import pre_process_images
+from softmaxModel import pre_process_images
 from trainer import BaseTrainer
-from assignment_code.softmaxModel import cross_entropy_loss, SoftmaxModel, one_hot_encode
+from softmaxModel import cross_entropy_loss, SoftmaxModel, one_hot_encode
 np.random.seed(0)
 
 class SoftmaxTrainer(BaseTrainer):
